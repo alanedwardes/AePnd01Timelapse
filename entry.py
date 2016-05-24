950204744904d9c9ca2f4cf4e81b3eaccb3ee136
Alan Edwardes <alan@alanedwardes.com>
2016-05-24 21:27:39 +0100

    Adds code to list an S3 bucket and print it out

--- entry.py
+++ entry.py
@@ -1,2 +1,9 @@
+import boto3
+
+BUCKET = 'ae-raspberry'
+PREFIX = 'pnd01/photos'
+
 def handler(event, context):
-  return 'hi'
+  bucket = s3.Bucket(BUCKET)
+  for object in bucket.objects.filter(Prefix=PREFIX):
+    print(object)
