3533b512b89e0cbde5f7686c1daee7b390b03b77
Alan Edwardes <alan@alanedwardes.com>
2016-05-24 21:29:55 +0100

    Adds s3 resource

--- entry.py
+++ entry.py
@@ -3,6 +3,8 @@ import boto3
 BUCKET = 'ae-raspberry'
 PREFIX = 'pnd01/photos'
 
+s3 = boto3.resource('s3')
+
 def handler(event, context):
   bucket = s3.Bucket(BUCKET)
   for object in bucket.objects.filter(Prefix=PREFIX):
