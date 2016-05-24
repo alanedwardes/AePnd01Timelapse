00b29b6f40abf4ad9039a48c53245a8cfb6ccf6c
Alan Edwardes <alan@alanedwardes.com>
2016-05-24 22:19:15 +0100

    Adds logging, adds executable bit setting on ffmpeg binary

--- entry.py
+++ entry.py
@@ -1,5 +1,6 @@
 import subprocess
 import boto3
+import stat
 import os
 
 FFMPEG = 'ffmpeg/ffmpeg'
@@ -12,16 +13,29 @@ s3 = boto3.resource('s3')
 
 def handler(event, context):
   bucket = s3.Bucket(BUCKET)
+  
+  print('Querying bucket for frame objects')
   objects = bucket.objects.filter(Prefix=PREFIX)
   
+  print('Setting executable permision on ffmpeg')
+  os.chmod(FFMPEG, os.stat(FFMPEG).st_mode | stat.S_IEXEC)
+  
   if not os.path.exists(FRAMES_OUTPUT):
+    print('Creating frames output directory')
     os.makedirs(FRAMES_OUTPUT)
   
+  print('Looping frame objects')
   for frame, object in enumerate(objects):
-    bucket.download_file(object.key, FRAMES_OUTPUT + '/' + '{0:03d}'.format(frame) + '.jpg')
-    
-  process = subprocess.Popen([FFMPEG, '-framerate 30', '-i ' + FRAMES_OUTPUT + '/%03d.jpg', '-c:v libx264', '-r 30', '-pix_fmt yuv420p', VIDEO_OUTPUT], shell=True, stdout=subprocess.PIPE)
-  print(process.stdout.read())
-  print(process.stderr.read())
+    filename = FRAMES_OUTPUT + '/' + '{0:03d}'.format(frame) + '.jpg'
+    print('Downloading frame ' + str(frame) + ' to ' + filename)
+    bucket.download_file(object.key, filename)
   
-  print os.path.exists(VIDEO_OUTPUT)
+  params = [FFMPEG, '-framerate 30', '-i ' + FRAMES_OUTPUT + '/%03d.jpg', '-c:v libx264', '-r 30', '-pix_fmt yuv420p', VIDEO_OUTPUT]
+  print('Invoking ' + ' '.join(params))
+  process = subprocess.Popen(params, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+  
+  print('ffmpeg stdout: ' + process.stdout.read())
+  
+  print('ffmpeg stderr: ' + process.stderr.read())
+  
+  print(os.path.exists(VIDEO_OUTPUT))
