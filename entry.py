4d8c455c529e8c11035411fbb72a5756175077dd
Alan Edwardes <alan@alanedwardes.com>
2016-05-25 01:00:39 +0100

    tweak params

--- entry.py
+++ entry.py
@@ -24,14 +24,14 @@ def download(frame, object):
 
 def handler(event, context):
   print('Querying bucket for frame objects')
-  objects = list(bucket.objects.filter(Prefix=PREFIX).all())[300:900]
+  objects = list(bucket.objects.filter(Prefix=PREFIX).all())[800:1200]
   
   if not os.path.exists(FRAMES_OUTPUT):
     print('Creating frames output directory')
     os.makedirs(FRAMES_OUTPUT)
   
   frame = 0
-  for object_batch in batch(objects, 14):
+  for object_batch in batch(objects, 12):
     print('Taking frame object batch')
     threads = []
     for object in object_batch:
