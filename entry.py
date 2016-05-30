b18d07127bd777024121a844dbf9076bfebfce75
Alan Edwardes <alan@alanedwardes.com>
2016-05-30 11:16:26 +0100

    Update entry.py

--- entry.py
+++ entry.py
@@ -64,6 +64,7 @@ def handler(event, context):
     '-i', FRAMES_OUTPUT + '/%05d.jpg',
     '-vcodec', 'libx264',
     '-preset', 'veryfast',
+    '-err_detect', 'explode',
     VIDEO_OUTPUT
   ]
   
@@ -81,7 +82,7 @@ def handler(event, context):
     'ACL': 'public-read'
   }))
   
-  topic.publish(
-      Message='https://{0}.s3.amazonaws.com/{1}'.format(BUCKET, timelapse),
-      Subject='Pond Timelapse Available for ' + yesterday.strftime('%A %d %b %Y')
-  )
+  #topic.publish(
+  #    Message='https://{0}.s3.amazonaws.com/{1}'.format(BUCKET, timelapse),
+  #    Subject='Pond Timelapse Available for ' + yesterday.strftime('%A %d %b %Y')
+  #)
